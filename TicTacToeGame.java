71ba76e7b674904c36b9ae793257e48ac2f81787
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-08 09:43:01 +0530

    [Kumar Kanishk] added : new feature in UC2 for taking turn and player

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class TicTacToeGame
 {
     static char[] board;
+    static char Turn;
     Scanner sc=new Scanner(System.in);
-    public void boardCreate()
+    public static void boardCreate()
     {
         board=new char[10];
         for(int i=1;i<10;i++)
@@ -11,9 +12,19 @@ public class TicTacToeGame
             board[i]=' ';
         }
     }
+    public static char newTurn()
+    {
+    	Scanner s=new Scanner(System.in);
+    	System.out.println("Enter P for player and C for Computer");
+    	char gamePlayer=s.next().charAt(0);
+    	System.out.println("Enter X or O as your turn");
+    	Turn=s.next().charAt(0);
+    	return Turn;
+    }
     public static void main(String args[])
     {
         TicTacToeGame obj=new TicTacToeGame();
         obj.boardCreate();
+        obj.newTurn();
     }
 }
