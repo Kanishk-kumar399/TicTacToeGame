db0087b13079bdc020f8d2966903f4948c705350
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-08 13:13:00 +0530

    [kumar kanishk] added UC6:new feature for tossing the coin

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class TicTacToeGame
 {
     static char[] board;
-    Scanner sc=new Scanner(System.in);
+    /*UC1*/
     public static void boardCreate()
     {
         board=new char[10];
@@ -11,14 +11,16 @@ public class TicTacToeGame
             board[i]=' ';
         }
     }
+    /*UC2*/
     public static char newTurn()
     {
     	Scanner s=new Scanner(System.in);
     	System.out.println("Enter X or O as your turn");
     	char Symbol=s.next().charAt(0);
-	System.out.println(Symbol+" is option for player");
+    	System.out.println(Symbol+" is for User");
     	return Symbol;
     }
+    /*UC3*/
     public static void displayBoard()
     {
 		System.out.println("  "+board[1] + " | " + board[2] + " | " + board[3]);
@@ -27,12 +29,13 @@ public class TicTacToeGame
 		System.out.println("--------------");
 		System.out.println("  "+board[7] + " | " + board[8] + " | " + board[9]);
     }
+    /*UC4*/
     public static int chooseIndex()
     {
         Scanner a=new Scanner(System.in);
         System.out.println("Enter an Index from 1 to 9");
         int i=a.nextInt();
-	while(true)
+            while(true)
             {
             if(board[i]!=' ')
              	{
@@ -43,6 +46,7 @@ public class TicTacToeGame
             }
             return i;
     }
+    /*UC5*/
     public static void makeDesiredMove(int indexForPlaying,char playerTurn)
     {
         if(board[indexForPlaying]==' ')
@@ -52,11 +56,28 @@ public class TicTacToeGame
             System.out.println("Unable to make the move");
         }
     }
+    /*UC6*/
+    public static int tossCoin()
+    {
+     int toss=(int)(Math.random()*10)%2;
+     final int HEADS=0;
+     final int TAILS=1;
+     if(toss==HEADS)
+     System.out.println("User Plays First");
+     else
+     System.out.println("Computer Plays First");
+     return toss;
+    }
     public static void main(String args[])
     {
+    	System.out.println("Welcome to Tic Tac Toe Game");
+    	Scanner sc=new Scanner(System.in);
         boardCreate();
-        newTurn();
-	displayBoard();
-	chooseIndex();
+        char choice=newTurn();
+        displayBoard();
+        tossCoin();
+        int index=chooseIndex();
+        makeDesiredMove(index,choice);
+        displayBoard();
     }
 }
