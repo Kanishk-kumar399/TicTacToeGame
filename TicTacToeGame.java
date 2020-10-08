6850ee16fac48affee613ad94961fe53714f6f31
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-08 11:15:28 +0530

    [kumar Kanishk] added UC4:new feature for choosing Index

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -17,11 +17,34 @@ public class TicTacToeGame
     	System.out.println("Enter X or O as your turn");
     	char Symbol=s.next().charAt(0);
 	System.out.println(Symbol+" is option for player");
-    	return symbol;
+    	return Symbol;
+    }
+    public static void displayBoard()
+    {
+		System.out.println("  "+board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("--------------");
+		System.out.println("  "+board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("--------------");
+		System.out.println("  "+board[7] + " | " + board[8] + " | " + board[9]);
+    }
+    public static int chooseIndex()
+    {
+        Scanner a=new Scanner(System.in);
+        System.out.println("Enter an Index from 1 to 9");
+        int i=a.nextInt();
+            if(board[i]==' ')
+            return i;
+            else
+            {
+                System.out.println("Choose Another Index! Index not free");
+                return chooseIndex();
+            }
     }
     public static void main(String args[])
     {
         boardCreate();
         newTurn();
+	displayBoard();
+	chooseIndex();
     }
 }
